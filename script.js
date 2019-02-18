f1fe9d3f49f4d4557669e9c87a80fd77c1779392
Gwenaelle Lohard <gwenlhd@gmail.com>
2019-02-18 10:15:40 +0100

    Trying to display the students cards of a given promotion

--- script.js
+++ script.js
@@ -118,3 +118,46 @@ fetch("http://api-students.popschool-lens.fr/api/promotions")
             })
             .catch()
     }
+
+
+// DISLPAYING THE STUDENTS
+
+// When we click on the button ShowList
+// We display the students of a specific promotion in #mydiv
+
+var showList = document.querySelector("#btnShowList");
+showList.addEventListener('click', getStudents);
+
+// When we click on the button, we get the students table with fetch
+function getStudents(event) {
+
+    var students = [];
+    var myDiv = document.querySelector("#mydiv");
+
+    console.log(SelectProm.value)
+
+        fetch("http://api-students.popschool-lens.fr/api/promotions/" + SelectProm.value)
+        .then(response => response.json())
+        .then(promotion => {
+    
+            // We display it into the console (raw datas)
+            console.log(students)
+            // We give a value to the students table (the value of the table within the object)
+            // students = myjson.students;
+
+            // We start to browse and display the students
+            promotion.students.forEach(function (student) {
+                console.log(student);
+                myDiv.innerHTML += `<div class="card" style="width: 18rem;">
+                <img class="card-img-top" src=".../100px180/" alt="">
+                    <div class="card-body">
+                    <h5 class="card-title"> ${student.firstname}</h5>
+                    <p class="card-text">Nom : ${student.lastname}</p>
+                    <p class="card-text">Nom : Sexe : ${student.sex}</p>
+                    <a href="#" class="btn btn-primary id="btnDeleteCard">Delete</a> <a href="#" class="btn btn-primary" id="btnModifyCard">Modify</a>
+                    </div>
+                    </div>`
+            });
+        });
+    }
+
